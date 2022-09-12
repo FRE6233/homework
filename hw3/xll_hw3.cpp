49a64ef7e5948e1fc24271276940d51f7073a529
keithtango <keith@bondtango.com>
2022-09-11 21:40:59 -0400

    hw3

---
+++ hw3/xll_hw3.cpp
@@ -0,0 +1,23 @@
+﻿// xll_template.cpp - Sample xll project.
+#include "xll_template.h"
+
+using namespace xll;
+
+AddIn xai_moneyness(
+	Function(XLL_DOUBLE, "xll_moneyness", "NORMAL.MONEYNESS")
+	.Arguments({
+		Arg(XLL_DOUBLE, "f", "is the forwar4d."),
+		Arg(XLL_DOUBLE, "s", "is the vol."),
+		Arg(XLL_DOUBLE, "k", "is the strike.")
+		})
+);
+double WINAPI xll_moneyness(double f, double s, double k)
+{
+#pragma XLLEXPORT
+	return moneyness(f, s, k);
+}
+
+//!!! implement xll_normal_pdf as NORMAL.PDF using normal_pdf
+//!!! implement xll_normal_cdf as NORMAL.CDF using normal_cdf
+//!!! implement xll_normal_cumulant as NORMAL.CUMULANT using normal_cumulant
+//!!! implement xll_black_put as BLACK.PUT using black_put
---
+++ hw3/xll_hw3.h
@@ -0,0 +1,47 @@
+// xll_template.h - common includes
+#pragma once
+#define _USE_MATH_DEFINES // for M_PI
+#include <cmath>
+#include "xll/xll/xll.h"
+
+// F = f exp(-s^2/2 + s Z) <= k if and only if Z <= (log(k/f) + s^2/2)/s
+inline double moneyness(double f, double s, double k)
+{
+	return (log(k / f) + s * s / 2) / s;
+}
+
+inline double normal_pdf(double z)
+{
+	return exp(-z * z / 2) / sqrt(2 * M_PI);
+}
+
+inline double normal_cdf(double z)
+{
+	return 0*z; //!!! use erf
+}
+
+// n-th derivative of the standard normal cumulant
+inline double normal_cumulant(double s, unsigned int n = 0)
+{
+	if (n == 0) {
+		return s * s / 2;
+	}
+	else if (n == 1) {
+		return s;
+	}
+	else if (n == 2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+inline double black_put(double f, double s, double k)
+{
+	double z = moneyness(f, s, k);
+	double N = 0*z; //!!! P(F <= k) = P(Z <= z)
+	double Ns = 0*z; //!!! P(Fe^{s^2/2} <= k) = P(Z + s <= z) = P(Z <= z - s)
+
+	return k * N - f * Ns;
+}
